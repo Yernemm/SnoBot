84d276d6df357651fd6570aaef43f96bf0e08fa6
Yernemm <yernemm@yernemm.xyz>
2019-08-02 02:57:25 +0100

    help only displays cmds with permission

--- commands/help.js
+++ commands/help.js
@@ -6,6 +6,7 @@ exports.cmdtype = () => {
 const desc = "Displays help and info about SnoBot."; //Short description of what the command does.
 const usage = "[command]"; //Any parameters required for command.
 const db = require('./../modules/db.js')
+const Enmap = require("enmap");
 exports.run = (data) => {
     let config = data.config;
     let client = data.client;
@@ -51,24 +52,29 @@ exports.run = (data) => {
     let count = 0;
 
     //INSTANTIATE CMD OBJECTS
-
-    fs.readdirSync(dir).forEach(file => {
-
-        if (file.endsWith(".js")) {
-            let cmdfile = require(`./${file}`);
-           
-            cmdObj[count] = new cmdClass(count);
-
-            cmdObj[count].id = count;
-            cmdObj[count].name = file.slice(0, -3);
-            cmdObj[count].desc = cmdfile.desc();
-            cmdObj[count].usage = cmdfile.use();
-            cmdObj[count].type = cmdfile.cmdtype();
-
-            count++;
+    //--------------------------------
+    let cmdCount = client.commands.keyArray().length;
+    client.commands.keyArray().forEach(c =>{
+        
+        console.log("yeehaw " + c)
+       db.checkPerms(data, c, res =>{
+        if(res){
+            console.log("yeenawnaw " + c)
+            let newCmdObj = {};
+            theCmd = client.commands.get(c);
+            newCmdObj.name = c;
+            newCmdObj.desc = theCmd.desc();
+            newCmdObj.usage = theCmd.use();
+            newCmdObj.type = theCmd.cmdtype();
+            cmdObj.push(newCmdObj);
+            console.log(cmdObj)
         }
-
+        cmdCount--;
+        if(cmdCount === 0) start()
+       })
+       
     })
+    //--------------------------------
 
     //CMD CLASS FUNCTIONS AND METHODS:
 
@@ -115,68 +121,82 @@ exports.run = (data) => {
 
 
 
+    function start(){
 
 
+console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+console.log(cmdObj)
 
-
-
-
-
-    var msg = "error";
-    switch (argsTxt) {
-        case null:
-        case "":
-        case " ":
-
-
-
-
-            msg = `**SnoBot, Made by Yernemm. https://yernemm.xyz**\r\nTo get help for a specific command, do ${config.prefix}help [command]\r\n\r\n`;
-            //Line below will be re-added when help command is finished for each command.
-            //TO-DO: finish this.
-            // msg += "__Use **>help [command]** to view specific help for that command.__\r\n";
-
-            msg += "Commands:\r\n"
-
-
-            var cmdInfoMsg = "";
-            console.log(getAllTypes());
-            getAllTypes().forEach( (type)=>{
-                cmdInfoMsg+= "**" + capitalizeFirstLetter(type) + "**\r\n```";
-                getAllCmdByType(type).forEach( (tcmd) =>{
-                    cmdInfoMsg += `${tcmd.name} `;
+        var msg = "error";
+        switch (argsTxt) {
+            case null:
+            case "":
+            case " ":
+    
+    
+    
+    
+                msg = `**SnoBot, Made by Yernemm. https://yernemm.xyz**\r\nTo get help for a specific command, do ${config.prefix}help [command]\r\n\r\n`;
+                //Line below will be re-added when help command is finished for each command.
+                //TO-DO: finish this.
+                // msg += "__Use **>help [command]** to view specific help for that command.__\r\n";
+    
+                msg += "Commands:\r\n"
+    
+    
+                var cmdInfoMsg = "";
+                console.log(getAllTypes());
+                getAllTypes().forEach( (type)=>{
+                    cmdInfoMsg+= "**" + capitalizeFirstLetter(type) + "**\r\n```";
+                    getAllCmdByType(type).forEach( (tcmd) =>{
+                        cmdInfoMsg += `${tcmd.name} `;
+                    });
+                    cmdInfoMsg+= "```\r\n";
+    
                 });
-                cmdInfoMsg+= "```\r\n";
+    
+                msg += "\r\n" + cmdInfoMsg;
+               
+    
+    
+    
+                  break;
+                default:
+    
+                let cmd = argsTxt;
+                try {
+                    let file = require(`./${cmd}.js`);
+    
+                    let de = file.desc();
+                    let us = file.use();
+                    msg = `**Help for ${config.prefix}${cmd}**\r\n${de}\r\nUsage: ${config.prefix}${cmd} ${us}`;
+                } catch (err) {
+                    msg = `Command \"${argsTxt}\" not found.\r\nUse **>help** to see available commands.`
+                }
+    
+                break;
+    
+    
+        }
 
-            });
-
-            msg += "\r\n" + cmdInfoMsg;
-           
 
 
 
-              break;
-            default:
 
-            let cmd = argsTxt;
-            try {
-                let file = require(`./${cmd}.js`);
-
-                let de = file.desc();
-                let us = file.use();
-                msg = `**Help for ${config.prefix}${cmd}**\r\n${de}\r\nUsage: ${config.prefix}${cmd} ${us}`;
-            } catch (err) {
-                msg = `Command \"${argsTxt}\" not found.\r\nUse **>help** to see available commands.`
-            }
-
-            break;
-
-
+        m.logSend(data, msg); //Method will send msg to user, and also log it in both console AND log channel.
     }
 
 
+
+
+
+
+
+   
+
+
     //--------------------------------------------------------------------
-    m.logSend(data, msg); //Method will send msg to user, and also log it in both console AND log channel.
+
     //m.log(config, client, message, msg); //Alternative will log msg without sending msg.
 }
 function onlyUnique(value, index, self) {
