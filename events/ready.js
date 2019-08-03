8489fb3558b4947a80b7c9d871d883e280476639
Yernemm <yernemm@yernemm.xyz>
2019-08-03 02:20:09 +0100

    url in status

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
     console.log(`Logged in as ${client.user.tag}`);
-    client.user.setPresence({ status: 'online', game: { name: `Yeet on them gamers.` } });
+    client.user.setPresence({ status: 'online', game: { name: `snobot.yernemm.xyz` } });
   };
