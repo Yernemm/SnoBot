6d0c525fe234a3e6cafa2bde1f0d07382adefec2
Yernemm <yernemm@yernemm.xyz>
2019-08-04 01:38:18 +0100

    adding panel

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        <div class="discordLoginBtn">
-          <a href="/api/discord/login">Login through discord</a>
-        </div>
-        </p>
+
+        <Route exact={true} path="/" render={()=>(
+          <div>
+          <p>Log into the web panel:</p>
+
+                  <p>
+                  <div class="discordLoginBtn">
+                    <a href="/api/discord/login">Login through discord</a>
+                  </div>
+                  </p>
+                  </div>
+        )}/>
+        <Route path="/panel" render={()=>(
+          <p>    <ul id="login"></ul></p>
+          
+        )}/>
+        
+        
+        
         
       </header>
     </div>
+    </Router>
   );
 }
 
