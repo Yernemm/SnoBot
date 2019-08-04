d53f339efb34fe218b04332fe383132a178bcab5
Yernemm <yernemm@yernemm.xyz>
2019-08-04 01:12:54 +0100

    react page

--- src/App.js
+++ src/App.js
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <div class="discordLoginBtn">
+          <a href="/api/discord/login">Login through discord</a>
+        </div>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
