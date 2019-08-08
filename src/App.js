e08cb088b75a9a628e420777438e9811a7299623
Yernemm <yernemm@yernemm.xyz>
2019-08-08 02:54:18 +0100

    nav with switches

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
     <div className="App">
     <div class="topnav">
-    <Route exact={true} path="/" render={()=>(
-      <div>
-  <Link class="active" to="/">Start Page</Link>
-  <Link to="/panel">Bot Panel</Link>
-  </div>
-    )}/>
-    <Route exact={true} path="/panel" render={()=>(
-      <div>
-  <Link  to="/">Start Page</Link>
-  <Link class="active" to="/panel">Bot Panel</Link>
-  </div>
-    )}/>
+    <Switch>
+        <Route exact={true} path="/" component={navStart}/>
+        <Route path="/panel" component={navPanel}/>       
+</Switch>
 </div>
       <header className="App-header">
         <div className="App-logo-container">
         <img class="w3-circle App-logo" src={logo} alt="logo" />
         </div>
+        <Switch>
         <Route exact={true} path="/" component={startPage}/>
         <Route path="/panel" component={panel}/>
-        
-        
+        <Route component={NoMatch} />
+        </Switch>
         <code>version 0</code>
         
       </header>
@@ -54,5 +47,32 @@ const panel = () => (
 <p>    <ul id="login"></ul></p>
   </div>
 )
+
+function NoMatch({ location }) {
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+      <Link class="active" to="/">Start Page</Link>
+    </div>
+  );
+}
+
+const navStart = () => (
+  <div>
+  <Link class="active" to="/">Start Page</Link>
+  <Link to="/panel">Bot Panel</Link>
+  </div>
+)
+
+const navPanel = () => (
+  <div>
+  <Link  to="/">Start Page</Link>
+  <Link class="active" to="/panel">Bot Panel</Link>
+  </div>
+)
+
+
 
 export default App;
