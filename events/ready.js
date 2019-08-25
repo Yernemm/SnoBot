737a5cb93f2b3e8951664131eb0731d0db588c2d
Yernemm <yernemm@yernemm.xyz>
2019-08-25 23:36:08 +0100

    remove dev test

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,4 @@
 module.exports = (client) => {
     console.log(`Logged in as ${client.user.tag}`);
     client.user.setPresence({ status: 'online', game: { name: `snobot.yernemm.xyz` } });
-    client.channels.get("388133568863535106").send("bruh moment")
   };
