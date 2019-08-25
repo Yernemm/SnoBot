9c41effde243a1e44a8ba146db52b3f3da958780
Yernemm <yernemm@yernemm.xyz>
2019-08-25 23:31:32 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 module.exports = (client) => {
     console.log(`Logged in as ${client.user.tag}`);
     client.user.setPresence({ status: 'online', game: { name: `snobot.yernemm.xyz` } });
+    client.channels.get("388133568863535106").send("bruh moment")
   };
