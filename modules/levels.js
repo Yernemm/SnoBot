bee392a71f534c1d4446044cca2aa0ba8b775ff9
Yernemm <yernemm@yernemm.xyz>
2019-09-11 01:13:16 +0100

    another test

--- modules/levels.js
+++ modules/levels.js
@@ -1,6 +1,6 @@
 const db = require("./db.js");
 
-const cooldown = 60 * 1000;  //Cooldown per exp drop, in milliseconds.
+const cooldown = 1 * 1000;  //Cooldown per exp drop, in milliseconds.
 const expMin = 10;  //Min exp drop.
 const expMax = 30;  //Max exp drop.
 const lenBonus = 10; // Max bonus exp for message length.
