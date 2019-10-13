051181332ae5411fd754f743ee8de405e0548910
Yernemm <yernemm@yernemm.xyz>
2019-10-13 01:11:25 +0100

    Update info.js

--- src/pages/info.js
+++ src/pages/info.js
@@ -19,7 +19,30 @@ export const infoPage = () => (
     <div>
     <div class="infoBodyContainer w3-card card">
       <div class="infoBody">
-    <p>This is a bot. </p>
+      <h2>Bot</h2>
+<p>A successor to YerBot, this bot offers a selection of both fun and useful commands:</p>
+<ul>
+<li>/translate -- Translate a message between any two languages.</li>
+<li>/trandom -- Translate a message between 10 random languages and back to English.</li>
+<li>/lastfm -- Get user music listening data from <a href="https://www.last.fm">last.fm</a></li>
+<li>/ow -- Display a user's Overwatch playime statistics</li>
+<li>/level -- Check your global and server exp and level.</li>
+<li>And more! -- Use /help to get a full list of commands.</li>
+</ul>
+<h2>Start Page</h2>
+<p>The start page shows the time, a Google search bar, and a set of useful website links for quick access.</p>
+<p>Set it as your browser's homepage for easy access.</p>
+<h2>More to come...</h2>
+<p>This website and bot are still in development. More features will come, including:</p>
+<ul>
+<li>More commands</li>
+<li>Configurable permission groups</li>
+<li>Role rewards for levels</li>
+<li>Moderation commands</li>
+<li>Mod mail</li>
+<li>Server spam detection and prevention</li>
+</ul>
+<p>For a full plan listing, check the <a href="https://trello.com/b/ic3OCNEa/snobot">Trello page.</a></p>
     </div>
     </div>
     
