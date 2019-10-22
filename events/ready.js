2464cfe7f06d5444584e45bd722bb575ef228b72
Yernemm <yernemm@yernemm.xyz>
2019-10-22 01:24:59 +0100

    send stats to top.gg

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,16 @@
+const DBL = require("dblapi.js");
+
 module.exports = (client) => {
-  
-    console.log(`Logged in as ${client.user.tag}`);
+  console.log(`Logged in as ${client.user.tag}`);
     let presence = ""
     if(client.ver.branch == "dev")
     presence += "[DEV] ";
     presence += `snobot.xyz  |  ${"v" + client.ver.num + "." + client.ver.build}`
     client.user.setPresence({ status: 'online', game: { name: presence } });
+
+    //dblapi
+    if(client.ver.branch !== "dev")
+    {
+      const dbl = new DBL(client.config.dblapiKey, client);  
+    }
   };
