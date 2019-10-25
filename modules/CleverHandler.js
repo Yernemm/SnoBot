15811c9f9d895f29a22d0471d10fae0654fdb0f6
Yernemm <yernemm@yernemm.xyz>
2019-10-25 02:38:17 +0100

    create CleverHandler.js

---
+++ modules/CleverHandler.js
@@ -0,0 +1,8 @@
+//This module contains the procedures for setting and loading clever channels.
+//It connects CleverChannel.js with the bot and saves data with db.js
+const CleverChannel = require('./CleverChannel.js');
+const db = require('./db.js');
+module.exports = {
+
+}
+
