e8e5e9ea2af487cfa6b036881d28d5a9d0b90b13
Yernemm <yernemm@yernemm.xyz>
2019-11-12 00:15:06 +0000

    start making Analytics

---
+++ modules/Analytics.js
@@ -0,0 +1,30 @@
+class Analytics {
+
+    /**
+     * Analytics class provides keeping track of the bot stats for devs.
+     * @param {string} channelId
+     *  The ID of the Discord channel in which to send analytics data. 
+     * @param {Object} client
+     *  The logged in Discord.JS client object.
+     */
+    constructor(client, channelId){
+        this._channelId = channelId;
+        this._client = client;
+    }
+
+    /**
+     * Starts sending analytics data to the channel at the given interval.
+     * @param {int} interval 
+     *  The time between each message in ms. Default is 1 hour.
+     */
+    start(interval = 1000 * 60 * 60){
+        
+    }
+
+    _sendingInterval(params) {
+        
+    }
+    
+}
+
+module.exports = Analytics;
