e36cc30ce9d7847ca785c84924bba63d50412bb3
Yernemm <yernemm@yernemm.xyz>
2019-12-31 04:52:31 +0000

    move all the old stuff

---
+++ legacy-2/build/precache-manifest.b8ba0101becfa1dc11b4f24f4b1a21a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927c29bf260aa36b3904924a16b708f7",
+    "url": "/panel/index.html"
+  },
+  {
+    "revision": "7c6d05fa57a87c84c9f2",
+    "url": "/panel/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/panel/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7c6d05fa57a87c84c9f2",
+    "url": "/panel/static/js/main.bb80d2b5.chunk.js"
+  },
+  {
+    "revision": "8e395e3ee8c67b99adb5",
+    "url": "/panel/static/js/runtime~main.b9326f19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/panel/static/media/logo.5d5d9eef.svg"
+  }
+]);
