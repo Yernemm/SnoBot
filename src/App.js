d93ad593b640a62aa6fdd8a698edc92f43353b6e
Yernemm <yrnemmizer@gmail.com>
2020-07-21 01:12:18 +0100

    yeah yeah uh uh

--- src/App.js
+++ src/App.js
@@ -21,18 +21,18 @@ class App extends React.Component {
   this.signOutState = this.signOutState.bind(this);
   }
 
-  
+
 
   componentDidMount() {
     const socket = socketIOClient();
-    socket.on('userData', data => 
+    socket.on('userData', data =>
     {
       console.log(data)
       this.setState(
           {
            discordUserData: data,
            authChecked: data.loaded,
-           loggedIn: data.loggedIn  
+           loggedIn: data.loggedIn
           }
         )
     });
@@ -48,7 +48,7 @@ class App extends React.Component {
       {
        discordUserData: undefined,
        authChecked: true,
-       loggedIn: false  
+       loggedIn: false
       }
     )
   }
@@ -57,51 +57,51 @@ class App extends React.Component {
     if(!this.state.discordUserData)this.state.discordUserData = "test"
   return (
     <Router>
-      
+
     <div className="App">
     <div id="particles-js" style={{ "top":0,"left":0,"position":"fixed",  "width": "100%", "height":"100%"}}></div>
 <div style={{"position":"absolute", "z-index":1, "width": "100%", "height":"100%"}}>
     <div class="topnav w3-card">
     <Route component={nav} />
     <code style={{color:"#fff", float:"right"}}>| {
-        
+
         this.state.loggedIn?
-        
+
         <span> <SignOut signOutState={this.signOutState} /> | Logged in as {this.state.discordUserData.username}.</span>
-        
+
         :
-        
+
         this.state.authChecked?
         <span> <a href="/api/discord/login">[<i class="fab fa-discord"></i> Login through Discord]</a></span>
         :
-        
+
         ""
-        
-        } | v{ver.ver.num}.{process.env.REACT_APP_GIT_VER} |</code>
+
+      } | v{ver.ver.num}.{process.env.REACT_APP_GIT_VER}{process.env.REACT_APP_GIT_BRANCH == "dev" ? "!! THIS IS A DEV SITE - BEWARE !!" : ""} |</code>
 </div>
 
 <div class="scrollbar-app">
     <div class="overflow">
       <header className="App-header">
 
-        
+
       <Switch>
       <Route exact={true} path="/" component={headerPage}/>
         <Route path="/start"/>
         <Route path="/panel"/>
         <Route component={NoMatch} />
         </Switch>
-        
+
       </header>
-      
+
       <Switch>
       <Route exact={true} path="/" component={infoPage}/>
 
-        <Route path="/start" render={(props) => 
+        <Route path="/start" render={(props) =>
   (<StartPage {...props} state={this.state}/>)
 }/>
 
-        <Route path="/panel" render={(props) => 
+        <Route path="/panel" render={(props) =>
   (<Panel {...props} state={this.state}/>)
 }/>
         </Switch>
@@ -110,7 +110,7 @@ class App extends React.Component {
         </div>
         </div>
     </div>
-  
+
     </Router>
   );
 }
@@ -132,7 +132,7 @@ function nav({ location }){
       "path": path
     })
   }
-  
+
 
   addLinkData("/", "Info");
   addLinkData("/start", "Start Page");
@@ -151,7 +151,7 @@ function nav({ location }){
     }
     links.push(<Link class={classes} to={d.path}>{d.name}</Link>);
   })
-  
+
   return (
     <div>
       {links}
@@ -172,7 +172,7 @@ class SignOut extends React.Component {
   handleClick() {
     this.props.signOutState()
   }
-  
+
   render() {
     return <a onClick={this.handleClick}>[Sign Out]</a>;
   }
