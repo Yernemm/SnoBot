fd042e07f25d71328445bab2b48f64c2e62eea31
Yernemm <yrnemmizer@gmail.com>
2020-07-21 03:45:59 +0100

    Update index-web.js

--- index-web.js
+++ index-web.js
@@ -108,6 +108,8 @@ app.get('/', function (req, res) {
              )
            });
 
+           console.log(guildArr);
+
            socket.emit('ownerAnalytics', {"servers": guildArr})
          }
         /*
